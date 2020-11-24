5432a8057791462dd95c8e08d6dd54c629afc425
HerosYellow <71410589+HerosYellow@users.noreply.github.com>
2020-11-24 04:01:33 +0300

    Add files via upload

---
+++ Mars's AIO v3/PIP Install.bat
@@ -0,0 +1,39 @@
+@echo off
+
+
+:start
+cls
+
+set python_ver=37
+python get-pip.py
+pip install sys
+pip install threading
+pip install platform
+pip install discord
+pip install asyncio
+pip install aiohttp
+pip install logging
+pip install json
+pip install os
+pip install re
+pip install io
+pip install time
+pip install json
+pip install random
+pip install discord
+pip install asyncio
+pip install datetime
+pip install requests
+pip install ctypes
+pip install aiohttp
+pip install colorama
+pip install subprocess
+pip install string 
+pip install random, string
+pip install functools
+pip install gtts
+pip install selenium
+pip install msvcrt
+
+
+pause
---
+++ Mars's AIO v3/launch.bat
@@ -0,0 +1,6 @@
+@echo off
+cls
+title Mars's AIO
+title Mars's AIO - Loading
+py -3.7 AIO.py
+pause
